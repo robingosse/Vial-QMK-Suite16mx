d91bbfa24c5ba7368f477c8d50b9d7ff1895fd00
Robin Gosse <robingosse2@gmail.com>
2026-08-12 19:36:12 -0230

    Map R83 diagnostic endpoints to printable keys

--- keyboards/opendeck/32/rev1/keymaps/vial/keymap.c
+++ keyboards/opendeck/32/rev1/keymaps/vial/keymap.c
@@ -1,12 +1,20 @@
 #include QMK_KEYBOARD_H
 
-// Suite16mx matrix-test keymap.
-// Distinct keycodes make it easy to verify every physical switch and matrix position.
+// Suite16mx R83 coupling-diagnostic keymap.
+// The first twelve synthetic matrix positions identify the twelve real matrix
+// GPIOs. Tap one physical key and note which diagnostic letters appear.
+//
+// R = GP27   E = GP28   W = GP26   Q = GP29
+// I = GP12   U = GP13   Y = GP14   T = GP15
+// F = GP0    D = GP1    S = GP2    A = GP3
+//
+// A healthy physical key should produce exactly TWO letters: one from
+// R/E/W/Q (row endpoint) and one from I/U/Y/T/F/D/S/A (column endpoint).
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x8(
-        KC_R,    KC_E,    KC_W,    KC_Q,    KC_I,    KC_U,    KC_Y,    KC_T,
-        KC_F,    KC_D,    KC_S,    KC_A,    KC_SCLN, KC_L,    KC_K,    KC_J,
-        KC_V,    KC_C,    KC_X,    KC_Z,    KC_COMM, KC_M,    KC_N,    KC_B,
-        KC_G,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_H
+        KC_R, KC_E, KC_W, KC_Q, KC_I, KC_U, KC_Y, KC_T,
+        KC_F, KC_D, KC_S, KC_A, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
     )
 };
