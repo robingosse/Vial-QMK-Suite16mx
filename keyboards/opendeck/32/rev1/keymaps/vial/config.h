19ca2c800e09d9f6a10b62aa9b5eebce3e7f6a8f
Robin Gosse <robingosse2@gmail.com>
2026-08-12 13:16:28 -0230

    Allow Vial unlock-free GPIO diagnostic

--- keyboards/opendeck/32/rev1/keymaps/vial/config.h
+++ keyboards/opendeck/32/rev1/keymaps/vial/config.h
@@ -2,5 +2,9 @@
 
 #define VIAL_KEYBOARD_UID {0x52, 0x6F, 0x62, 0x69, 0x6E, 0x31, 0x36, 0x59}
 
+// Diagnostic-only: allow Vial access without a physical unlock combo while
+// the switch matrix itself is under test. Remove this for production firmware.
+#define VIAL_INSECURE
+
 #define VIAL_UNLOCK_COMBO_ROWS {0, 0}
 #define VIAL_UNLOCK_COMBO_COLS {1, 0}
