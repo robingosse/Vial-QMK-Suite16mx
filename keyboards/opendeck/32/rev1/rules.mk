20dee948dd203a77cc008bc9f7490be5c526e5ea
Robin Gosse <robingosse2@gmail.com>
2026-08-12 18:19:52 -0230

    Restore normal Suite16mx matrix scanner

--- keyboards/opendeck/32/rev1/rules.mk
+++ keyboards/opendeck/32/rev1/rules.mk
@@ -9,10 +9,4 @@ OLED_ENABLE = no
 ENCODER_ENABLE = no
 AUDIO_ENABLE = no
 
-# Temporary Suite16mx bring-up diagnostic: bypass the normal row/column scanner
-# and use rev1/matrix.c to probe GP0..GP12 for any switch closure in either
-# electrical direction.
-CUSTOM_MATRIX = lite
-SRC += matrix.c
-
 LTO_ENABLE = yes
